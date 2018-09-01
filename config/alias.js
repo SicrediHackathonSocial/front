3f568c76a41a1ceb02170a15825559e37e71d200
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2018-09-01 12:39:01 -0300

    oi

---
+++ config/alias.js
@@ -0,0 +1,7 @@
+const paths = require('./paths')
+
+module.exports = {
+  'app-components': `${paths.appSrc}/ui/components`,
+  'app-services': `${paths.appSrc}/services`,
+  'app-pages': `${paths.appSrc}/ui/pages`
+}
