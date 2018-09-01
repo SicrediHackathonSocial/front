a0241eb103576f818977ce2d2cc9b4376de69bf3
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2018-09-01 19:02:55 -0300

    objetivo

--- src/ui/components/image/images/menu.js
+++ src/ui/components/image/images/menu.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 export default (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 124 124">
-    <g>
-      <path d="M112,6H12C5.4,6,0,11.4,0,18s5.4,12,12,12h100c6.6,0,12-5.4,12-12S118.6,6,112,6z" />
-      <path d="M112,50H12C5.4,50,0,55.4,0,62c0,6.6,5.4,12,12,12h100c6.6,0,12-5.4,12-12C124,55.4,118.6,50,112,50z" />
-      <path d="M112,94H12c-6.6,0-12,5.4-12,12s5.4,12,12,12h100c6.6,0,12-5.4,12-12S118.6,94,112,94z" />
-    </g>
+  <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z" fill="#003049"/>
   </svg>
 )
