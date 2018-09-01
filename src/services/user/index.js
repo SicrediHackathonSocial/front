d2da11ebd8443cba720de35d6a174ef58277ec83
João Fidellis <joao.fidellis@cwi.com.br>
2018-09-01 17:29:04 -0300

    Adicionando lógica de auth

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,7 +1,17 @@
 import BaseService from '../_base'
+import axios from "axios"
 
 export class UserService extends BaseService {
   constructor() {
     super('user')
+  }
+
+  login(loginDto) {
+    const promise = axios.post(this.baseUrl + '/login', loginDto)
+    promise.then(token => {
+      localStorage.setItem(this.localStorageAuthLocation, token)
+    })
+
+    return promise
   }
 }
