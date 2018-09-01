80e81998377996d8ff45fb6295682e06d54a3d86
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2018-09-01 15:29:55 -0300

    fix

--- src/ui/components/sidenav/nav-button/index.jsx
+++ src/ui/components/sidenav/nav-button/index.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 
+const MenuLink = ({ match, to, label }) => (
+  <li
+    className={`sidenav__menu__item ${
+      match ? 'sidenav__menu__item--active' : ''
+    }`}
+  >
+    <Link to={to}>
+      <span className="sidenav__menu__item__link">{label}</span>
+    </Link>
+  </li>
+)
+
 export class NavButton extends Component {
   render() {
     return (
-      <Route path={this.props.to} exact={this.props.activeOnlyWhenExact} children={({ match }) => (
-        <li className={`sidenav__menu__item ${match ? 'sidenav__menu__item--active' : ''}`}>
-          <Link to={this.props.to}>
-            <span className="sidenav__menu__item__link">{this.props.label}</span>
-          </Link>
-        </li>
-      )}/>
+      <Route
+        path={this.props.to}
+        exact={this.props.activeOnlyWhenExact}
+        children={({ match }) => (
+          <MenuLink match={match} to={this.props.to} label={this.props.label} />
+        )}
+      />
     )
   }
 }
