2beca8b7d3b983f67de239327c83b7ddec7a998b
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2018-09-02 05:18:06 -0300

    foi

--- src/ui/pages/visualizar-objetivos/meus-objetivos/index.jsx
+++ src/ui/pages/visualizar-objetivos/meus-objetivos/index.jsx
@@ -1,10 +1,61 @@
 import React, { Component } from 'react'
-import { ObjetivosUsuario } from 'app-components'
+import { ObjetivosUsuario, Image } from 'app-components'
+
+import { Route, Link } from 'react-router-dom'
 
 import './styles.css'
 
 export class MeusObjetivosPage extends Component {
   render() {
-    return (<ObjetivosUsuario></ObjetivosUsuario>)
+    return (
+      <div>
+        <ObjetivosUsuario></ObjetivosUsuario>
+
+        <div className="home-footer">
+          <div className="footer-item">
+            <Route
+              path="/meus-objetivos"
+              exact={true}
+              children={({ match }) => (
+                <Link to="/meus-objetivos" label={this.props.label}>
+                  <div className={match && 'footer-item--active'}>
+                    { Image.ICONS.Profile }
+                    <span>Meus projetos</span>
+                  </div>
+                </Link>
+              )}
+            />
+          </div>
+          <div className="footer-item">
+            <Route
+              path="/objetivos-compartilhados"
+              exact={true}
+              children={({ match }) => (
+                <Link match={match} to="/objetivos-compartilhados" label={this.props.label}>
+                  <div className={match && 'footer-item--active'}>
+                    { Image.ICONS.Profile }
+                    <span>Compartilhados</span>
+                  </div>
+                </Link>
+              )}
+            />
+          </div>
+          <div className="footer-item">
+            <Route
+              path="/meus-projetos"
+              exact={true}
+              children={({ match }) => (
+                <Link match={match} to="/meus-projetos" label={this.props.label}>
+                  <div className={match && 'footer-item--active'}>
+                    { Image.ICONS.Profile }
+                    <span>ONGs</span>
+                  </div>
+                </Link>
+              )}
+            />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
--- src/ui/pages/visualizar-objetivos/objetivos-compartilhados/index.jsx
+++ src/ui/pages/visualizar-objetivos/objetivos-compartilhados/index.jsx
@@ -1,10 +1,61 @@
 import React, { Component } from 'react'
-import { ObjetivosCompartilhados } from 'app-components'
+import { ObjetivosCompartilhados, Image } from 'app-components'
+
+import { Route, Link } from 'react-router-dom'
 
 import './styles.css'
 
 export class ObjetivosCompartilhadosPage extends Component {
   render() {
-    return (<ObjetivosCompartilhados></ObjetivosCompartilhados>)
+    return (
+      <div>
+        <ObjetivosCompartilhados></ObjetivosCompartilhados>
+
+        <div className="home-footer">
+          <div className="footer-item">
+            <Route
+              path="/meus-objetivos"
+              exact={true}
+              children={({ match }) => (
+                <Link to="/meus-objetivos" label={this.props.label}>
+                  <div className={match && 'footer-item--active'}>
+                    { Image.ICONS.Profile }
+                    <span>Meus projetos</span>
+                  </div>
+                </Link>
+              )}
+            />
+          </div>
+          <div className="footer-item">
+            <Route
+              path="/objetivos-compartilhados"
+              exact={true}
+              children={({ match }) => (
+                <Link match={match} to="/objetivos-compartilhados" label={this.props.label}>
+                  <div className={match && 'footer-item--active'}>
+                    { Image.ICONS.Profile }
+                    <span>Compartilhados</span>
+                  </div>
+                </Link>
+              )}
+            />
+          </div>
+          <div className="footer-item">
+            <Route
+              path="/meus-projetos"
+              exact={true}
+              children={({ match }) => (
+                <Link match={match} to="/meus-projetos" label={this.props.label}>
+                  <div className={match && 'footer-item--active'}>
+                    { Image.ICONS.Profile }
+                    <span>ONGs</span>
+                  </div>
+                </Link>
+              )}
+            />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
