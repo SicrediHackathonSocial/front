febc261458e6fe0c8beb4bba05ba224b68e53087
João Fidellis <joao.fidellis@cwi.com.br>
2018-09-01 22:01:00 -0300

    Iniciando home

---
+++ src/ui/components/objetivos/objetivos-compartilhados/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ObjetivosCompartilhados extends Component {
+    // constructor(props) {
+    //     super(props)
+    // }
+
+    render() {
+        return <h1>objetivos compartilhados</h1>
+    }
+}
---
+++ src/ui/components/objetivos/objetivos-usuario/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ObjetivosUsuario extends Component {
+    // constructor(props) {
+    //     super(props)
+    // }
+
+    render() {
+        return <h1>objetivos usuário</h1>
+    }
+}
