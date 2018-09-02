ff8248566ab609622a8152a9c49f14ab2c150d5b
robsonrosa <robsonrosa@cwi.com.br>
2018-09-02 08:26:17 -0300

    layout detalhe objetivo

--- src/ui/components/objetivos/detalhe-objetivo/detalhe-principal/index.jsx
+++ src/ui/components/objetivos/detalhe-objetivo/detalhe-principal/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import './styles.css'
+
 export class DetalhePrincipal extends Component {
     constructor(props) {
         super(props)
@@ -8,7 +10,16 @@ export class DetalhePrincipal extends Component {
     render() {
         return (
             <div className="detalhe-principal-objetivo">
-                comprar um carro
+                <h1 className="detalhe-principal-objetivo-titulo">Comprar um carro</h1>
+
+                <div className="detalhe-principal-objetivo-values">
+                    <h2 className="detalhe-principal-objetivo-reached">R$ 483,00</h2>
+                    <h3 className="detalhe-principal-objetivo-target"> / R$ 500,00</h3>
+                </div>
+
+                <div className="detalhe-principal-objetivo-progress-wrapper">
+                    <div className="detalhe-principal-objetivo-progress"></div>
+                </div>
             </div>
         )
     }
